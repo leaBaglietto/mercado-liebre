fe451868e61d25721220d72aafb42f127d1fbb7d
leaBaglietto <leandro.baglietto@gmail.com>
2021-01-05 20:06:08 -0300

    form

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get ('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'))
 })
 
-app.get ('/', (req, res) => {
+app.get ('/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/register.html'))
 })
 
